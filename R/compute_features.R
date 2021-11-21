f9743287a9944222dd26fc3846a214aeaf65aa1b
Juraj Szitas <szitas.juraj13@gmail.com>
2021-11-21 22:10:02 +0100

    Improve feature calculation

--- R/compute_features.R
+++ R/compute_features.R
@@ -1,48 +1,27 @@
-soothsayer_feature_set <- list( entropy,
-                                lumpiness,
-                                nonlinearity,
-                                hurst,
-                                stability,
-                                box_pierce,
-                                acf_features,
-                                intermittent,
-                                spectral,
-                                arch_stat,
-                                longest_flat_spot,
-                                n_crossing_points,
-                                ljung_box,
-                                unitroot_kpss,
-                                feasts::unitroot_ndiffs,
-                                feasts::unitroot_nsdiffs,
-                                unitroot_pp,
-                                feasts::shift_kl_max,
-                                feasts::shift_level_max,
-                                feasts::shift_var_max,
-                                 # heterogeneity,
-                                positive,
-                                negative,
-                                zeros,
-                                continuous,
-                                count,
-                                tslength,
-                                period,
-                                boxcox_lambda,
-                                catch22_feat
-)
-
+#' Compute features for a tsibble
+#'
+#' @param x A tsibble.
+#' @param ... Additional arguments.
+#' @return A data.frame wth computed time series features, with ncol features and
+#' nrow individual time series.
+#' @export
 compute_features <- function(x,...) {
   UseMethod("compute_features",x)
 }
-compute_features.tbl_ts <- function( x, feature_set = soothsayer_feature_set, values_from = "value", ... ) {
-  # just a lambda function
-
-  safe_set <- purrr::map( feature_set, ~ purrr::possibly(.f = .x, otherwise = NaN)  )
-
-  transformer <- function( .x ) {
-    .features <- purrr::map( safe_set,
-                             function(.f) .f( .x[[values_from]] ))
-    unlist(.features)
+#' @export
+compute_features.tbl_ts <- function( x, feature_set = soothsayer_feature_set, values_from = NULL,... ) {
+  # get the name of the target variable
+  if( is.null( values_from ) ) {
+    values_from <- tsibble::measured_vars(x)
+    if( length(values_from) != 1 ) {
+      rlang::abort(
+        paste0( "Cannot identify a single column to use for feature calculation ",
+                "- please use the **values_from** argument of this function." ))
+    }
   }
+  transformer <- generate_feature_transformer( feature_set, values_from )
+  # if we have an unkeyed table (as is case from within the soothsayer object)
+  # we would crash here on the keyed version - thus we add a check.
   if( length(tsibble::key_vars(x)) != 0) {
     features <- x %>%
       as.data.frame() %>%
@@ -60,13 +39,29 @@ compute_features.tbl_ts <- function( x, feature_set = soothsayer_feature_set, va
         furrr::future_map( transformer ) %>%
         dplyr::bind_rows()
     })
-
+    # the early return saves us an otherwise unnecessary else clause
     return(cbind(keys, features))
   }
+  # for a single unkeyed dataset, we can just call the transformer directly
   features <- transformer(as.data.frame(x))
   feature_names <- names(features)
-
+  # a bit of coercion magic so that we get the correctly formatted, correctly
+  # named table
   features <- as.data.frame(matrix( transformer(as.data.frame(x)), nrow = 1))
   colnames(features) <- feature_names
   return(features)
+}
+# we could probably have this within compute_features, but for reproducibility
+# and cleanliness, we already have it separated out
+generate_feature_transformer <- function( feature_set, values_from ) {
+  # just a wrapper so that the functions return NaN rather than a random error
+  safe_set <- purrr::map( feature_set, ~ purrr::possibly(.f = .x, otherwise = NaN)  )
+  # then we generate a closure which specifies what we will be calculating -
+  # this also enables as to easily use future_map
+
+  transformer <- function( .x ) {
+    .features <- purrr::map( safe_set,
+                             function(.f) .f( .x[[values_from]] ))
+    unlist(.features)
+  }
 }
