732f6074e3153091788c49289a07080935bf0357
Juraj Szitas <szitas.juraj13@gmail.com>
2021-12-06 01:13:31 +0100

    Separate out methods, and implement new method for forecasting

--- R/methods.R
+++ R/methods.R
@@ -26,24 +26,27 @@ generate.soothsayer <- function (x, new_data = NULL, h = NULL, specials = NULL, 
     tsibble::as_tsibble(index = "Month")
 
 }
-# merge_distribution_forecasts <- function( dist_fcsts, weights = NULL ) {
-#
-#   if(is.null(weights)) {
-#     weights <- rep(1, length(dist_fcsts))/length(dist_fcsts)
-#   }
-#   ind_var <- tsibble::index_var(dist_fcsts[[1]])
-#   dates <- dist_fcsts[[1]][[ind_var]]
-#
-#   distrs <- purrr::map( dist_fcsts,  )
-#
-#
-#
-#
-#   distributional::dist_mixture()
-#
-#
-#
-# }
+merge_distribution_forecasts <- function( dists, weights = NULL ) {
+
+  if(is.null(weights)) {
+    weights <- rep(1, length(dists))/length(dists)
+  }
+  # note that since purrr::pmap_dist doesnt exist, we have to use the list version
+  # and then index into the list... which is ugly, but it works
+  merged_dists <- purrr::pmap(dists,
+                              ~ distributional::dist_mixture(.x,
+                                                             weights = weights)
+                              )[[1]]
+  return( merged_dists )
+}
+
+# to be fair, this is a method over a fable, but I do not want to write a generics for it
+get_distribution <- function(x) {
+  distr <- purrr::map_lgl( x, distributional::is_distribution )
+  distr <- names(distr)[ which(distr) ]
+
+  c(x[,distr])
+}
 
 #' @importFrom fabletools forecast
 #' @export
@@ -53,15 +56,16 @@ forecast.soothsayer <- function( object,
                                  bootstrap = FALSE,
                                  times = 100,
                                  ... ) {
-  purrr::map( object[["models"]],
+  fcsts <- purrr::map( object[["models"]],
               function(.x) {
                 fabletools::forecast(
                   .x[[1]],
                   new_data = new_data,
                   bootstrap = bootstrap,
-                  times = times, ...)
+                  times = times, ...) %>%
+                  get_distribution()
               }
-  )[[1]]
-  # global_res_fcst <<- res
-  # dplyr::bind_rows(res)#[[1]]
+  )
+  # for now mix the distributions with equal weights
+  merge_distribution_forecasts(fcsts, NULL)
 }
