bfb8cbb7127e758fcb426f06b363355c39910060
Juraj Szitas <szitas.juraj13@gmail.com>
2021-12-10 01:48:50 +0100

    New weighed generation method (much improved) now works

--- R/methods.R
+++ R/methods.R
@@ -3,7 +3,8 @@
 generate.soothsayer <- function (x, new_data = NULL, h = NULL, specials = NULL, times = 1, bootstrap = FALSE, seed = NULL,
                                  ...)
 {
-  combinator <- specials$combinator[[1]]
+  weights <- x[["model_weights"]]
+  names(weights) <- names(x[["models"]])
   generated_distrs <- purrr::imap( x[["models"]],
                                    function(model, name) {
                                      dplyr::bind_cols(
@@ -17,17 +18,24 @@ generate.soothsayer <- function (x, new_data = NULL, h = NULL, specials = NULL, 
                                          ...),
                                        model = name)
                                    })
-  dplyr::bind_rows( purrr::map( generated_distrs,
-                                dplyr::as_tibble ) ) %>%
+  dists <- purrr::imap( generated_distrs, function(dist, name) {
+    dist %>%
+    dplyr::mutate( .sim = .sim * weights[name]) %>%
+      tsibble::as_tibble()
+  }) %>%
+    dplyr::bind_rows()
+  dists %>%
     dplyr::group_by(.data$Month) %>%
-    dplyr::summarise( .sim = combinator(.data$.sim),
+    dplyr::summarise( .sim = sum(.data$.sim),
                       .rep = unique(.data$.rep)
     ) %>%
     tsibble::as_tsibble(index = "Month")
 
 }
 merge_distribution_forecasts <- function( dists, weights = NULL ) {
-
+  if( length(dists) == 1 ) {
+    return(dists[[1]])
+  }
   if(is.null(weights)) {
     weights <- rep(1, length(dists))/length(dists)
   }
@@ -66,6 +74,29 @@ forecast.soothsayer <- function( object,
                   get_distribution()
               }
   )
+  # weights <- combine_greedy_stacking( object[["models"]] )
+  dsts <<- fcsts
   # for now mix the distributions with equal weights
-  merge_distribution_forecasts(fcsts, NULL)
+  merge_distribution_forecasts(fcsts, weights = NULL )
 }
+
+# IDEA: pdqr to the rescue!
+# # Create a list of pdqr-functions
+# norm_list <- list(
+#   as_d(dnorm), as_d(dnorm, mean = 2, sd = 0.25), as_d(dnorm, mean = 4, sd = 0.5)
+# )
+#
+# # Form a mixture with custom weights
+# norm_mix <- form_mix(norm_list, weights = c(0.6, 0.2, 0.2))
+#
+# # Compute 95% highest density region
+# (norm_hdr <- summ_hdr(norm_mix, level = 0.95))
+# #>          left      right
+# #> 1 -1.82442072 2.53095750
+# #> 2  3.19649819 4.79334429
+#
+# # Visualize
+# plot(norm_mix, main = "95% highest density region for normal mixture")
+# region_draw(norm_hdr)
+
+
