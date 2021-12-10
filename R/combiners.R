fc302936956e0b82bfbe1a05d4db8f4d7caf01cb
Juraj Szitas <szitas.juraj13@gmail.com>
2021-12-10 01:47:49 +0100

    Improve combination methods - new model special for combining now just computes weights

---
+++ R/combiners.R
@@ -0,0 +1,41 @@
+#' @rdname combiners
+#' Get combinations weights for model combinatiions
+#'
+#' @desccription Combination functions which return combination weights
+#' @param .models The fitted models - this is a named or unnamed list of fitted fable compatible models.
+#' @param prior_weights Prior weights to use for combinations - see details.
+#' @param metric The metric to use when using greedy stacking (defaults to rmse).
+#' @param ... Additional parameters (currently not implemented).
+#' @return A vector of length .models containing the individual weights
+#' @details Prior weights can be passed to combiner_custom and they will be the
+#' only weights computed by that combiner. Other combiners take the average of prior_weights
+#' and whatever weights they output.
+#' @export
+combiner_greedy_stacking <- function(.models, prior_weights = NULL, metric = rmse, ... ) {
+
+  Z <- .models %>%
+    purrr::map(~ fitted(.x[[1]]) ) %>%
+    purrr::map(as.data.frame) %>%
+    purrr::map( ~ .x[[".fitted"]] ) %>%
+    dplyr::bind_cols() %>%
+    as.matrix
+  y <- .models[[1]][[1]][["data"]]
+  measured_var <- tsibble::measured_vars(y)
+  y <- y[[measured_var]]
+
+  greedy_weights <- greedy_stacking( y, Z, metric = metric,... )
+
+  colMeans( rbind( greedy_weights, prior_weights))
+}
+#' @rdname combiners
+#' @export
+combiner_mean <- function(.models, prior_weights = NULL, ... ) {
+
+  avg_weights <- rep(1, length(.models))/length(.models)
+  colMeans( rbind( avg_weights, prior_weights))
+}
+#' @rdname combiners
+#' @export
+combiner_custom <- function(.models, prior_weights = NULL, ... ) {
+  prior_weights/sum(prior_weights)
+}
--- R/greedy_stack.R
+++ R/greedy_stack.R
@@ -1,12 +1,16 @@
 
 rmse <- function( true, predicted ) {
-  sqrt(mean( (true - predicted)^2))
+  sqrt(mean( (na.omit(true - predicted))^2))
 }
 
-# greedy stacking of models, or determining model weights greedily using 
-# arbitrary metrics 
+mae <- function( true, predicted ) {
+  mean( abs(na.omit(true - predicted)))
+}
+
+# greedy stacking of models, or determining model weights greedily using
+# arbitrary metrics
 # see: https://bmcresnotes.biomedcentral.com/articles/10.1186/s13104-020-4931-7
-greedy_stacking <- function( y, Z, metric = rmse, max_iter = 100 ) {
+greedy_stacking <- function( y, Z, metric = rmse, criterium_better = which.min, max_iter = 100 ) {
   # s in the original paper
   step <- 0
   # x in the original paper
@@ -14,19 +18,21 @@ greedy_stacking <- function( y, Z, metric = rmse, max_iter = 100 ) {
   P <- rep(0, nrow(Z))
   weights <- rep(0, ncol(Z))
   while(TRUE) {
+
     step <- step + 1
-    
+
     for( col in seq_len(ncol(Z)) ) {
       evaluations[col] <- metric( y, (P + Z[,col])/step )
     }
-    eval_max <- which.max(evaluations)
-    # they use an X[,j_max] in the original paper, but I am 99.9% 
-    # sure they actually mean Z instead of X (since there is no X in the 
+
+    eval_max <- criterium_better(evaluations)
+    # they use an X[,j_max] in the original paper, but I am 99.9%
+    # sure they actually mean Z instead of X (since there is no X in the
     # algorithm anywhere, and Z actually makes sense)
     P <- P + Z[,eval_max]
     # update weights
     weights[ eval_max ] <- weights[eval_max] + 1
-    # they do not go into more detail on the convergence criterion so maybe it 
+    # they do not go into more detail on the convergence criterion so maybe it
     # can be improved further
     if( step > max_iter ) {
       break
