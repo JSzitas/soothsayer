2a3764c10d60b052738acc9c94f31f0d3edb4f0a
Juraj Szitas <szitas.juraj13@gmail.com>
2021-12-16 00:04:33 +0100

    Add model resolution util

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,27 @@ fail_if_not_cond <- function( cond, msg, type = "input_field_null" ) {
     rlang::abort(message = msg, class = type)
   }
 }
+# this is a bit awful, but restructuring is hard
+model_resolver <- function( rule_models = NULL,
+                            oracle_models = NULL,
+                            which_model = "all",
+                            resolution = "both", ... ) {
+  if( resolution == "both" ) {
+    # if all, perform union
+    if( which_model == "all" ) {
+      return(unique(c(rule_models, oracle_models)))
+    }
+    # otherwise find the intersection and throw the first model
+    return( intersect( rule_models, oracle_models)[1] )
+  }
+  else if(resolution == "rule") {
+    models <- rule_models
+  }
+  else {
+    models <- oracle_models
+  }
+  if(which_model == "first") {
+    models <- models[1]
+  }
+  return(models)
+}
