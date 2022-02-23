ecbc77a0022d3f921e5f82b83ea72c106ea88068
Juraj Szitas <szitas.juraj13@gmail.com>
2022-02-23 15:35:02 +0100

    Add lazyness to soothsayer - now can be used after fitting other models, to enable easier ensembling

---
+++ R/add_soothsayer.R
@@ -0,0 +1,90 @@
+#' Create soothsayer from existing models
+#'
+#' @description Add a soothsayer to an existing **mable** wthout reestimating models
+#' @param mable The mable to add soothsayer to.
+#' @param ... The soothsayer to add - supports multiple soothsayers.
+#' @return A mable.
+#'
+
+add_soothsayer <- function(mable, ...) {
+  .data <- mable[[1]][[1]][["data"]]
+
+  soothsayers <- list(...)
+  fixed_soothsayers <- purrr::map(soothsayers, function(soothsayer) {
+    fml <- suppressWarnings(as.character(soothsayer[["formula"]]))
+    fml[2] <- paste0(fml[2], " + .noestimate(TRUE)")
+    fml <- paste0(fml[1], "(", fml[2], ")", collapse = "")
+
+    soothsayer[["formula"]] <- rlang::as_quosure(stats::formula(fml))
+
+    return(soothsayer)
+  })
+
+  new_mable <- fabletools::model(.data, !!!soothsayers)
+
+  # add fitted models to new_mable
+  for (col in colnames(new_mable)) {
+    #get fitting rules (ie the models to use)
+    fit_rules <- new_mable[[col]][[1]][["fit"]][["fit_rules"]]
+    # fit whatever isnt available yet
+    model_fits <- resolve_models(fit_rules, mable, .data, ...)
+    # and access the combiner function
+    combiner <- new_mable[[col]][[1]][["fit"]][["specials"]][["combiner"]]
+
+    # resolve model weights
+    model_weights <- if (length(model_fits) > 1) {
+      # if these are not NULL they are oracle weights
+      oracle_weights <- new_mable[[col]][[1]][["fit"]][["model_weights"]]
+      combiner[[1]](model_fits, oracle_weights)
+    } else {
+      1
+    }
+    # get fitted values and compute weighed fit
+    fitted_vals <- purrr::map(
+      model_fits,
+      function(model_fit) {
+        as.data.frame(stats::fitted(model_fit[[1]]))[[".fitted"]]
+      }
+    )
+    weighed_fits <- purrr::map2(
+      fitted_vals, model_weights,
+      function(fit, weight) {
+        fit * weight
+      }
+    )
+    fit_val <- purrr::reduce(weighed_fits, .f = `+`)
+    # compute residuals of weighed fit
+    resid <- unlist(as.data.frame(.data)[[tsibble::measured_vars(.data)]]) - fit_val
+
+    new_mable[[col]][[1]][["fit"]] <- structure(
+      list(
+        model_fits = model_fits,
+        fit_rules = fit_rules,
+        model_weights = model_weights,
+        fitted_vals = fit_val,
+        resid = resid
+      ),
+      class = "soothsayer"
+    )
+  }
+  return(dplyr::bind_cols(mable, new_mable))
+}
+
+resolve_models <- function(fit_rules, models, .data, ...) {
+  already_fitted_models <- models[, which(names(models) %in% names(fit_rules))]
+  unfitted_models <- names(fit_rules)[!(names(fit_rules) %in% names(models))]
+  unfitted_models <- fit_rules[unfitted_models]
+
+  target <- tsibble::measured_vars(.data)
+  model_defs <- purrr::map(unfitted_models, ~ .x(!!rlang::sym(target)))
+  model_fits <- fabletools::model(.data,
+    !!!model_defs,
+    .safely = TRUE
+  )
+  # bind old and new models, and reorder them so they appear in the same order
+  # as they would if fitting was done right away.
+  mdls <- dplyr::bind_cols(already_fitted_models, model_fits)[names(fit_rules)]
+  # convert to mable - note that the model variable seems redundant (as mdls is
+  # named) but is nonetheless necessary (not using it results in obscure bugs)
+  return( fabletools::as_mable(mdls, model = names(mdls)))
+}
--- R/soothsayer.R
+++ R/soothsayer.R
@@ -1,4 +1,5 @@
-train_soothsayer <- function(.data, specials, ...) {
+estimate_soothsayer <- function( .data, specials, ... ) {
+
   feature_set <- specials$feature_set[[1]]
 
   target <- tsibble::measured_vars(.data)
@@ -25,8 +26,8 @@ train_soothsayer <- function(.data, specials, ...) {
   oracle_weights <- NULL
   if (!is.null(oracle)) {
     matched_models_oracle <- ifelse(!is.null(oracle),
-      predict(oracle, feature_df),
-      NULL
+                                    predict(oracle, feature_df),
+                                    NULL
     )
     if (oracle[["emit_type"]] == "weights") {
       oracle_weights <- c(matched_models_oracle)
@@ -43,39 +44,53 @@ train_soothsayer <- function(.data, specials, ...) {
   models_to_fit <- aliases[matched_models]
   model_defs <- purrr::map(models_to_fit, ~ .x(!!rlang::sym(target)))
 
-  model_fits <- fabletools::model(.data,
-    !!!model_defs,
-    .safely = TRUE
-  )
-  model_weights <- if (length(model_fits) > 1) {
-    specials$combiner[[1]](model_fits, oracle_weights)
-  } else {
-    1
+  if( specials[[".noestimate"]][[1]] ) {
+    return( c(list(
+      model_fits = NULL,
+      fit_rules = models_to_fit,
+      model_weights = oracle_weights,
+      resid = NULL,
+      fitted_vals = NULL),
+      specials = list(specials)
+    ))
   }
-  fitted_vals <- purrr::map(
-    model_fits,
-    function(model_fit) {
-      as.data.frame(stats::fitted(model_fit[[1]]))[[".fitted"]]
+  else {
+    model_fits <- fabletools::model(.data,
+                                    !!!model_defs,
+                                    .safely = TRUE
+    )
+    model_weights <- if (length(model_fits) > 1) {
+      specials$combiner[[1]](model_fits, oracle_weights)
+    } else {
+      1
     }
-  )
-  weighed_fits <- purrr::map2(
-    fitted_vals, model_weights,
-    function(fit, weight) {
-      fit * weight
-    }
-  )
-  fit_val <- purrr::reduce( weighed_fits, .f = `+` )
-  resid <- unlist(as.data.frame(.data)[[target]]) - fit_val
+    fitted_vals <- purrr::map(
+      model_fits,
+      function(model_fit) {
+        as.data.frame(stats::fitted(model_fit[[1]]))[[".fitted"]]
+      }
+    )
+    weighed_fits <- purrr::map2(
+      fitted_vals, model_weights,
+      function(fit, weight) {
+        fit * weight
+      }
+    )
+    fit_val <- purrr::reduce( weighed_fits, .f = `+` )
+    resid <- unlist(as.data.frame(.data)[[target]]) - fit_val
+  }
+  return(list( model_fits = model_fits,
+               fit_rules = models_to_fit,
+               model_weights = model_weights,
+               fitted_vals = fit_val,
+               resid = resid ))
+}
+
+
+train_soothsayer <- function(.data, specials, ...) {
 
   structure(
-    list(
-      features = feature_df,
-      rules = fit_rules,
-      models = model_fits,
-      fitted = fit_val,
-      resid = resid,
-      model_weights = model_weights
-    ),
+    estimate_soothsayer(.data, specials),
     class = "soothsayer"
   )
 }
@@ -125,7 +140,8 @@ specials_soothsayer <- fabletools::new_specials(
     # This model doesn't support exogenous regressors, time to error.
     stop("Exogenous regressors aren't supported by `soothsayer()`")
   },
-  .required_specials = c("feature_set", "model_aliases", "combiner", "resolution")
+  .noestimate = function(no_estimate = FALSE){ return(no_estimate) },
+  .required_specials = c("feature_set", "model_aliases", "combiner", "resolution",".noestimate")
 )
 #' Soothsayer model
 #'
